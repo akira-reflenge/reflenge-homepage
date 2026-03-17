4f080aca3dde767e7d5e6a78c07af26aadc88236
akira-reflenge <work.akiaki.reflenge@gmail.com>
2026-03-17 20:28:12 +0900

    修正作業:APIキーの変更,自社製品ページの作成

--- src/library/microcms.ts
+++ src/library/microcms.ts
@@ -1,10 +1,8 @@
 import { createClient } from "microcms-js-sdk";
 
 export const client = createClient({
-  serviceDomain: "d2bgcbqohs", 
-  // ↓ここを、Q473で終わる英数字だけにしてください。
-  // 「ここに貼り付け」などの日本語や、前後のスペースは一切含めないのがコツです。
-  apiKey: "fyYfLw46H144lbTu3quFileC2AroOMRWQ473", 
+  serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
+  apiKey: import.meta.env.MICROCMS_API_KEY,
 });
 
 // 型定義はそのまま
@@ -31,6 +29,22 @@ export type Product = {
   id: string;
   title: string;
   content: string; // リッチエディタの中身（HTML文字列）が入ります
+  eyecatch: {
+    url: string;
+    width: number;
+    height: number;
+  };
+  category: {
+    id: string;
+    name: string;
+  } | null;
+  publishedAt: string;
+};
+
+export type OurProduct = {
+  id: string;
+  title: string;
+  content: string;
   eyecatch: {
     url: string;
     width: number;
