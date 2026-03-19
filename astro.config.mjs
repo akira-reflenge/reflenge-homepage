bbf9fd87713fa44056cc5cdcb57790833267e6d1
akira-reflenge <work.akiaki.reflenge@gmail.com>
2026-03-19 22:41:58 +0900

    修正作業:投稿記事の即時反映

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
   // 開発バーをプロジェクト全体で無効化
   devToolbar: {
     enabled: false
